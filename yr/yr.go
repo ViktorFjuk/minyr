e29ec68b50e04c94251b9d37ffd572c210143744
ViktorFjuk <vikfju@gmail.com>
2023-04-12 18:45:36 +0000

    forskjellig endringer

--- yr/yr.go
+++ yr/yr.go
@@ -1,1 +1,45 @@
-yr.go
+package yr
+
+
+import (
+
+        "fmt"
+        "strconv"
+        "strings"
+        "errors"
+        "log"
+        "os"
+        "io"
+        "bufio"
+        "github.com/ViktorFjuk/funtemps/conv"
+
+)
+
+
+func LesAntallLinjerFil(filename string) int{
+
+src, err := os.Open("/home/ViktorFjuk/minyr/yr/kjevik-temp-celsius-20220318-20230318.csv")
+
+if err != nil {
+
+        log.Fatal(err)
+
+}
+
+defer src.Close()
+
+scanner := bufio.NewScanner(src)
+
+antall := 0
+
+for scanner.Scan() {
+
+antall++
+
+}
+
+
+return antall
+
+}
+
--- yr/yr_test.go
+++ yr/yr_test.go
@@ -1,1 +1,43 @@
-yr test
+package yr
+
+
+
+import (
+
+        "testing"
+        "bufio"
+	"encoding/csv"
+	"os"
+	"strings"
+	"io"
+)
+
+
+func  TestLesAntallLinjerFil(t *testing.T) {
+
+        type test struct {
+
+        input string
+
+        want int
+}
+
+
+
+tests := []test{
+
+{input: "kjevik-temp-celsius-20220318-20230318.csv", want:  1656},
+
+}
+
+
+for _, tc := range tests {
+
+        got := LesAntallLinjerFil(tc.input)
+
+                if got != tc.want {
+                        t.Errorf("%v: want %v, got %v,", tc.input, tc.want, got)
+}
+}
+}
+
