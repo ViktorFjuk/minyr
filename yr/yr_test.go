4cae55fc9f057d644f134993b2c2448c9edb9270
ViktorFjuk <vikfju@gmail.com>
2023-04-12 20:59:55 +0000

    f endringer i test filen.

--- yr/yr_test.go
+++ yr/yr_test.go
@@ -41,3 +41,21 @@ for _, tc := range tests {
 }
 }
 
+func TestKonverteringAvLinjer(t *testing.T) {
+
+	type test struct {
+
+	input string
+
+	want int
+}
+
+tests := []test{
+
+{input: "Kjevik;SN39040;18.03.2022 01:50;6", want: "Kjevik;SN39040;18.03.2022 01:50;42.8"},
+{input: "Kjevik;SN39040;07.03.2023 18:20;0", want: "Kjevik;SN39040;07.03.2023 18:20;32.0"},
+{input: "Kjevik;SN39040;08.03.2023 02:20;-11", want: "Kjevik;SN39040;08.03.2023 02:20;12.2"},
+
+}
+
+
